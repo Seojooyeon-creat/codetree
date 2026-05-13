3c9a994854dd2bdbf649d19546e4975034bb210b
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-13 15:39:09 +0900

    [Passed] 달에서 무게 구하기 (8ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a = 13;
+    double b = 0.165;
+    
+    cout << fixed;
+    cout.precision(6);
+    
+    // 출력
+    cout << a << " * " << b << " = " << a * b << endl;
+    return 0;
+}
+
