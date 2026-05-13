e40bec368ced6a0b1d4e709da945208c7ba5a3eb
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-13 15:17:34 +0900

    [Passed] 문자 변경하기 (8ms, 0MB)

---
+++ trail0/문자 변경하기/change-charater.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    char a = 'C';
+    
+      // 값 변경
+      a = 'T';
+    
+    // 출력
+    cout << a << endl;
+    return 0;
+}
+
