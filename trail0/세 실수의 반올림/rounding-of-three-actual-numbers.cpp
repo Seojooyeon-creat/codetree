3168c3cfaea4141ee345187110e53c03185f428d
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-13 15:48:01 +0900

    [Passed] 세 실수의 반올림 (8ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    double a, b, c;
+
+    // 입력
+    cin >> a >> b >> c;
+    
+    cout << fixed;
+    cout.precision(3);
+    
+    // 출력
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
+
