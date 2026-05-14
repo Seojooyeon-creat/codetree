5dd7e50dc96b9f15f086c138c175d7a909c49815
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-14 13:28:54 +0900

    [Passed] 정수 입력받아 배수 출력 (8ms, 0MB)

---
+++ trail0/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, i;
+    cin >> n;
+    for(i=1; i<6; i++){
+        cout << n * i << " ";
+    }
+    
+    return 0;
+}
