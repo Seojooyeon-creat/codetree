2f65638552fd4c7ca13ec5666b3dccbbe240aba7
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-14 13:23:04 +0900

    [Passed] 문자 출력하기 (8ms, 0MB)

---
+++ trail0/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    char a;
+    cin >> a;
+    for(i = 0; i < 8; i++){
+        cout << a;
+    }
+    return 0;
+}
