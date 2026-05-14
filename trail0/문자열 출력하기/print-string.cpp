96a95695cdca142ca2ff82d50b650c6d26db923f
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-14 13:24:44 +0900

    [Passed] 문자열 출력하기 (8ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int i;
+    cin >> n;
+    for(i=0; i<n; i++){
+        cout << "LeebrosCode" << "\n";
+    }
+    return 0;
+}
