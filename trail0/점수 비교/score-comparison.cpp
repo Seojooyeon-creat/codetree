d2e8658f0475d4bc1de244ce3dbe54a2c27c42ee
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-17 20:25:11 +0900

    [Passed] 점수 비교 (8ms, 0MB)

---
+++ trail0/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    int c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    if(a > c and b > d) cout << 1;
+    else cout << 0;
+    return 0;
+}
