035c8a0e6a9ee24154add17fb1727b981483c270
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-05-30 16:44:39 +0900

    [Passed] 두 정수에 대한 연산값 (51ms, 16MB)

---
+++ trail2/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+def modi(a, b):
+    if a > b:
+        a += 25
+        b *= 2
+    else:
+        b += 25
+        a *= 2
+    print(a, b)
+
+modi(a, b)
