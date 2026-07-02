adae029e1b309db70771f6fc6dee8832b88c6d3c
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-07-02 21:48:40 +0900

    [Passed] 정수 선언하고 뺄셈 (99ms, 11MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a, b;
+        a = 97;
+        b = 13;
+        System.out.println("97 - 13 = " + (a - b));
+    }
+}
