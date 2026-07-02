6de64274160d3f3e0dbf10812a4a44b4dfe2a8a7
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-07-02 21:52:39 +0900

    [Passed] 세 정수형 변수 선언 (94ms, 9MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 7, b = 23, c = 30;
+
+        // 출력
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
+
