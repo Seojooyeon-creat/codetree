fff3bdf8ce23e35bee40b232a0470f869b271539
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-07-02 22:22:59 +0900

    [Passed] 문자열 순서 바꾸기 (130ms, 10MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String a, b;
+        a = sc.next();
+        b = sc.next();
+
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
