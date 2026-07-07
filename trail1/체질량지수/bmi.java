dfd1c371eae16336f8269126b7ac133dfe955dff
SuperSeonic <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-07-07 21:12:10 +0900

    [Passed] 체질량지수 (140ms, 10MB)

---
+++ trail1/체질량지수/bmi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int h, w, b;
+        h = sc.nextInt();
+        w = sc.nextInt();
+        b = (10000 * w) / (h * h);
+
+        System.out.println(b);
+
+        if(b >= 25){
+            System.out.println("Obesity");
+        }
+
+
+
+    }
+}
